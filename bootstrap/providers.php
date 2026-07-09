aa6a945c3c98ac57addd7e4650acef88e77caf91
campos <nonesovile96@hotmail.com>
2026-07-09 16:47:48 -0300

    Revert "Filament"
    This reverts commit b725ccec990d875705b2d4e79b44d43a4ff315a0.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\ProjetosPanelProvider::class,
+    AppServiceProvider::class,
 ];
