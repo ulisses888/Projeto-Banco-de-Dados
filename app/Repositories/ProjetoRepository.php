012252eec89fd387ca6a59074ccf1d4bfaa5ab9d
campos <nonesovile96@hotmail.com>
2026-07-09 23:23:58 -0300

    Adicionar métodos de alunos em Projeto

--- app/Repositories/ProjetoRepository.php
+++ app/Repositories/ProjetoRepository.php
@@ -11,38 +11,55 @@ class ProjetoRepository
         return DB::select('SELECT * FROM projetos');
     }
 
-    public static function projeto_por_codigo($codigo): array
+    public static function projeto_por_codigo(int $codigo): array
     {
         return DB::select('SELECT * FROM projetos WHERE codigo = ?', [$codigo]);
     }
 
-    public static function adicionar_projeto($codigo, $nome, $eixo, $status, $ano)
+    public static function adicionar_projeto(int $codigo, string $nome, string $eixo, string $status, string $ano)
     {
         DB::insert('INSERT INTO projetos VALUES (?, ?, ?, ?, ?)', [$codigo, $nome, $eixo, $status, $ano]);
     }
 
-    public static function deletar_projeto($codigo)
+    public static function deletar_projeto(int $codigo)
     {
         DB::delete('DELETE FROM projetos WHERE codigo = ?', [$codigo]);
     }
 
-    public static function atualizar_nome($codigo, $nome)
+    public static function atualizar_nome(int $codigo, string $nome)
     {
         DB::update('UPDATE projetos SET nome = ? WHERE codigo = ?', [$nome, $codigo]);
     }
 
-    public static function atualizar_eixo($codigo, $eixo)
+    public static function atualizar_eixo(int $codigo, string $eixo)
     {
         DB::update('UPDATE projetos SET eixo = ? WHERE codigo = ?', [$nome, $eixo]);
     }
 
-    public static function atualizar_status($codigo, $status)
+    public static function atualizar_status(int $codigo, string $status)
     {
         DB::update('UPDATE projetos SET status = ? WHERE codigo = ?', [$status, $codigo]);
     }
 
-    public static function atualizar_ano($codigo, $ano)
+    public static function atualizar_ano(int $codigo, int $ano)
     {
         DB::update('UPDATE projetos SET ano = ? WHERE codigo = ?', [$ano, $codigo]);
+    }
+
+    // Relações
+    
+    public static function alunos(int $codigo): array
+    {
+        return DB::select('SELECT * FROM alunos NATURAL JOIN aluno_participa WHERE codigo = ?', [$codigo]);
+    }
+
+    public static function adicionar_aluno(int $codigo, int $matricula, bool $bolsista)
+    {
+        DB::insert('INSERT INTO aluno_participa VALUES (?, ?, ?)', [$matricula, $codigo, $bolsista]);
+    }
+
+    public static function remover_aluno(int $codigo, int $matricula)
+    {
+        DB::delete('DELETE FROM aluno_participa WHERE codigo = ? AND matricula = ?', [$codigo, $matricula]);
     }
 }
