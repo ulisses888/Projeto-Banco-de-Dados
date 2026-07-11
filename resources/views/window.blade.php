448fe8818d1f9ce02cfa60d51f7723987b0f39ca
Érick Machado <zubretax2@outlook.com>
2026-07-10 22:17:15 -0300

    Prototipo De UI (sem nenhuma funcionalidade)

---
+++ resources/views/window.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title style="color: white;">Document</title>
+
+<!--Cabeçalho -->
+</head>
+
+<style>
+        h1{
+            color: white; 
+            text-align: center;
+            font-family: 'American Typewriter', serif;
+            font-size: 300%;
+    
+        }
+        p{
+            color: white; 
+            text-align: center;
+        
+        }
+    </style>
+
+    <h1 style="color: white; text-align: center;">Banco De dados</h1>
+<body bgcolor="1A1818">
+
+<!--estilo da página -->
+    <style>
+        h1{
+            color: #1A1818;
+        }
+        p{
+            color: white; 
+            text-align: left;
+            padding-left: 20px;
+            background-color: #2c2a2a; width: 110px; height: 20px; border-radius: 5px;
+            margin: 20px;
+        }
+    </style>
+
+
+    <!--Área de texto -->
+    <br>
+    <br>
+    <button onlick=""style="background-color: #2c2a2a; width: 100px; height: 20px; border-radius: 5px; color: white; margin-left: 20px;">Teste</button>
+    <p>Area de testes</p>
+
+    <iframe style="padding-left: 200px; padding-top: -50px; border: none;" src="resources/views/Table_area.blade.php" height="600" width="1100"></iframe>
+
+
+</body>
+</html>
