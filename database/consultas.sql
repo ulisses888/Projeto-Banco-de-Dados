6f417c1c8fa8ddb87275e565ef853359f186ed98
campos <nonesovile96@hotmail.com>
2026-07-14 20:24:34 -0300

    Melhorar consultas

--- database/consultas.sql
+++ database/consultas.sql
@@ -1,20 +1,28 @@
 USE projetos;
 
+# Alunos do projeto 2
 SELECT matricula, nome, bolsista FROM alunos NATURAL JOIN aluno_participa WHERE codigo = 1;
 
-SELECT siape, nome, funcao FROM servidores NATURAL JOIN servidor_participa WHERE codigo = 1;
+# Servidores do projeto 2
+SELECT siape, nome, funcao FROM servidores NATURAL JOIN servidor_participa WHERE codigo = 2;
 
-SELECT cpf, nome FROM outros NATURAL JOIN outro_participa WHERE codigo = 1;
+# Outros participantes do projeto 2
+SELECT cpf, nome FROM outros NATURAL JOIN outro_participa WHERE codigo = 2;
 
+# Selecionar programas e seus projetos
 SELECT codigo_programa, programas.nome, codigo_projeto, projetos.nome, status FROM programas
 INNER JOIN programa_contem ON programa_contem.codigo_programa = programas.codigo 
 INNER JOIN projetos ON programa_contem.codigo_projeto = projetos.codigo;
 
+# Selecionar bolsistas
 SELECT matricula, nome, bolsista FROM alunos NATURAL JOIN aluno_participa WHERE bolsista = 1;
 
+# Selecionar não bolsistas
 SELECT matricula, nome, bolsista FROM alunos NATURAL JOIN aluno_participa WHERE bolsista = 0;
 
-SELECT acoes.codigo, titulo, genero, acoes.status FROM acoes
+# Selecionar todas as ações
+SELECT acoes.codigo, projetos.nome AS nome_projeto, titulo, genero, acoes.status FROM acoes
 INNER JOIN projetos ON acoes.codigo_projeto = projetos.codigo;
 
+# Selecionar todos os coordenadores
 SELECT siape, nome, funcao FROM servidor_participa NATURAL JOIN servidores WHERE funcao = 'Coordenador';
